a12838cb933dc16c768c502190d444005f71dea9
GlassMC <fuzz@glass-mc.com>
2025-05-04 06:24:09 -0700

    Delete java/net/glassmc/mapartcopyright directory

--- java/net/glassmc/mapartcopyright/commands/SubCommand.java
+++
@@ -1,8 +0,0 @@
-package net.glassmc.mapartcopyright.commands;
-
-import org.bukkit.command.CommandSender;
-
-public interface SubCommand {
-    String getName();
-    void execute(CommandSender sender, String[] args);
-}
